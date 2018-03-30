55561802ea3dbc9451094623e3c0ce100187476b
Kevin Jennison <kevin.jennison1@gmail.com>
2018-03-29 18:01:25 -0700

    Add ability to create a DB with an object store and test a transaction

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
     super(props)
     this.firebaseIndexedDbName = 'firebaseLocalStorageDb'
     this.firebaseObjectStoreName = 'firebaseLocalStorage'
+    this.firebaseObjectStoreKeyPath = 'fbase_key'
+    this.dbVersion = (
+      window.localStorage.getItem('dbVersion')
+        ? parseInt(window.localStorage.getItem('dbVersion'), 10)
+        : 1
+    )
   }
 
   initFirebase () {
@@ -24,21 +30,70 @@ class App extends Component {
 
   async deleteFirebaseIndexedDB () {
     console.log('Deleting Firebase IndexedDB...')
+    window.localStorage.removeItem('dbVersion')
     await idb.delete(this.firebaseIndexedDbName)
     console.log('Finished deleting Firebase IndexedDB')
   }
 
+  async getFirebaseIndexedDB () {
+    const db = await idb.open(this.firebaseIndexedDbName)
+    return db
+  }
+
+  async createFirebaseIndexedDB (upgradeCallback = () => {}) {
+    const version = this.dbVersion ? this.dbVersion : ''
+    const self = this
+    const db = await idb.open(this.firebaseIndexedDbName, version, (upgradeDb) => {
+      self.dbVersion += 1
+      window.localStorage.setItem('dbVersion', self.dbVersion)
+      upgradeCallback(upgradeDb)
+    })
+    return db
+  }
+
   async createFirebaseIndexedDBWithoutObjectStore () {
     console.log('Creating Firebase IndexedDB...')
-    window.performance.mark('idb-pre-open')
-    await idb.open(this.firebaseIndexedDbName)
-    window.performance.mark('idb-post-open')
-    window.performance.measure('idb-time-to-open', 'idb-pre-open', 'idb-post-open')
-    const perfMeasures = window.performance.getEntriesByName('idb-time-to-open')
-    if (perfMeasures.length) {
-      console.log('Time to open IDB:', perfMeasures.slice(-1)[0].duration)
-    }
-    console.log('Finished creating Firebase IndexedDB without object store')
+    const db = await this.createFirebaseIndexedDB(async upgradeDb => {
+      // Try deleting the object store (it may or may not exist)
+      try {
+        upgradeDb.deleteObjectStore(this.firebaseObjectStoreName)
+        console.log('Deleted object store from DB')
+      } catch (e) {
+        console.warn('No object store existed, so we did not delete one')
+      }
+    })
+    console.log('Finished opening Firebase IndexedDB without object store')
+    db.close()
+  }
+
+  async createFirebaseIndexedDBWithObjectStore () {
+    console.log('Creating Firebase IndexedDB...')
+    const self = this
+    const db = await this.createFirebaseIndexedDB(async upgradeDb => {
+      // Try creating the object store (it may or may not exist)
+      try {
+        // https://github.com/firebase/firebase-js-sdk/blob/1b3ba41f7c5207907a32601e552168e29281948e/packages/auth/src/storage/indexeddb.js#L185
+        upgradeDb.createObjectStore(this.firebaseObjectStoreName, {
+          keyPath: self.firebaseObjectStoreKeyPath
+        })
+        console.log('Added object store to DB')
+      } catch (e) {
+        console.warn('An object store already existed, so we did not add one')
+      }
+    })
+    console.log('Finished opening Firebase IndexedDB')
+    db.close()
+  }
+
+  async testOpenDBTransaction () {
+    console.log('Opening Firebase DB transaction (this will stall on Firefox if there\'s an error)...')
+    const db = await this.getFirebaseIndexedDB()
+    await db
+      .transaction(this.firebaseObjectStoreName)
+      .objectStore(this.firebaseObjectStoreName)
+      .get('foo')
+    console.log('Finished opening transaction')
+    db.close()
   }
 
   render () {
@@ -72,7 +127,19 @@ class App extends Component {
           onClick={this.createFirebaseIndexedDBWithoutObjectStore.bind(this)}
           style={buttonStyle}
         >
-          Create Firebase IndexedDB without object store
+          Create DB without object store
+        </div>
+        <div
+          onClick={this.createFirebaseIndexedDBWithObjectStore.bind(this)}
+          style={buttonStyle}
+        >
+          Create DB with object store
+        </div>
+        <div
+          onClick={this.testOpenDBTransaction.bind(this)}
+          style={buttonStyle}
+        >
+          Test opening a transaction
         </div>
       </div>
     )
